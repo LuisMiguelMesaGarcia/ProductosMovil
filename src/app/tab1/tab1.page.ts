80d5eac49b637f1489785a3c272ffbb4ee39e742
HarolFlorez <harolflorezr924@gmail.com>
2023-05-01 18:47:55 -0500

    subir cambio

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { SERVICESService } from "../services/services.service";
 import { CommonModule } from '@angular/common';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-tab1',
@@ -11,21 +12,38 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent,CommonModule],
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page {
   constructor(public http:SERVICESService) {}
+
+  productosFav: any [] = [];
 
   public list:any=[];
 
   ngOnInit(): void {
     this.function_get_PRODUCTOS();
+    
   }
-
 
   public function_get_PRODUCTOS(){
     this.http._getProducto_().subscribe((resp:any)=>{
+      resp.products.forEach((element: { isFavorite: boolean; }) => {
+        element.isFavorite = false;
+        
+      });
        this.list=resp.products;
-       console.log(this.list);
+      //  console.log(this.list);
     })
+  }
+
+  statusFavorite(item: any){
+    this.list[item.id -1].isFavorite = !this.list[item.id -1].isFavorite;
+    this.saveFavoritos(item);
+  
+  }
+
+  saveFavoritos(item: any){
+    this.productosFav.push(item);
+    localStorage.setItem('MisFav', JSON.stringify(this.productosFav));
   }
   
 }
